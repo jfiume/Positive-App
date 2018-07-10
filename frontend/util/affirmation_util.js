fc0a0b218fd6e3c9f36bdd1180d0a33feda8a9a1
Joe Fiume <joefiume88@gmail.com>
2018-07-09 17:32:37 -0700

    triggering fetch based off of __DEV__

--- frontend/util/affirmation_util.js
+++ frontend/util/affirmation_util.js
@@ -1,9 +1,12 @@
 // // Asynchronously fetch a random affirmation
 export const fetchRandom = async function() {
   try {
-    const response = await fetch("http://localhost:5000/affirmation/random");
+    if (__DEV__) {
+      var response = await fetch("http://localhost:5000/affirmation/random");
+    } else {
+      var response = await fetch("/affirmation/random");
+    }
     const affirmation = await response.json();
-    console.log(affirmation);
     return affirmation;
   } catch (e) {
     console.error("API request raised an error:", e);
