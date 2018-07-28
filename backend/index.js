5364bf5045ec8dba647dbf2ddb7cc692a44816ec
Joe Fiume <joefiume88@gmail.com>
2018-07-28 13:54:01 -0700

    further testing for heroku working

--- backend/index.js
+++ backend/index.js
@@ -16,19 +16,19 @@ var affirmation = require('./routes/affirmation.js');
 
 var uri = require('../.gitignore/mongodb_settings.js');
 // Create a database variable outside of the database connection callback to reuse the connection pool in your app.
-var db;
-
-// Connect to the database before starting the application server.
-mongodb.MongoClient.connect(uri || "mongodb://localhost:27017/my_db", function (err, client) {
-  if (err) {
-    console.log(err);
-    process.exit(1);
-  }
-
-  // Save database object from the callback for reuse.
-  db = client.db();
-  console.log("Database connection ready");
-});
+// var db;
+//
+// // Connect to the database before starting the application server.
+// mongodb.MongoClient.connect(uri || "mongodb://localhost:27017/my_db", function (err, client) {
+//   if (err) {
+//     console.log(err);
+//     process.exit(1);
+//   }
+//
+//   // Save database object from the callback for reuse.
+//   db = client.db();
+//   console.log("Database connection ready");
+// });
 
 
 app.use('/user', user);
